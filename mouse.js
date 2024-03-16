e487fbcf8686bcaf8e6c872ac8f97322b7ea4469
macosta2015 <macosta2015@my.fit.edu>
2024-03-16 00:33:32 -0400

    Adjust Max X and Y
    In the code you are able to adjust the maximum X and Y.

--- mouse.js
+++ mouse.js
@@ -2,30 +2,32 @@ const puppeteer = require('puppeteer');
 
 (async () => {
     try {
-        console.log('Launching browser...');
+        // Launch the browser with custom viewport size
         const browser = await puppeteer.launch({
+            defaultViewport: { width: 800, height: 800 }, // Initial size of the browser window
             headless: false, // Launch Chrome with visible browser window
             userDataDir: '/Users/marioacosta/Library/Application Support/Google/Chrome/Profile 1' // Path to your user data directory
         });
 
-        console.log('Opening new page...');
-        const newPage = await browser.newPage();
+        // Open a new page
+        const page = await browser.newPage();
 
-        // Navigate to the URL
-        console.log('Navigating to the URL...');
-        await newPage.goto('https://google.com', { waitUntil: 'networkidle0' });
+        // Navigate to Google
+        await page.goto('https://google.com', { waitUntil: 'networkidle0' });
 
         // Capture mouse movements and log coordinates
-        await newPage.evaluate(() => {
+        await page.evaluate(() => {
             document.addEventListener('mousemove', (event) => {
                 console.log(`Mouse coordinates: X = ${event.clientX}, Y = ${event.clientY}`);
             });
         });
 
+        // Keep the page open for further interaction or observation
         console.log('Move the mouse over the page to see the coordinates...');
 
-        // Keep the page open
+        // Prevent the script from exiting
         await new Promise(() => { });
+
     } catch (error) {
         console.error('An error occurred:', error);
     }
